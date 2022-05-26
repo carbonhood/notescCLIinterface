ff45adae7c9dfae903677be704c2bd9d22c07ff0
carbonhood <106286968+carbonhood@users.noreply.github.com>
2022-05-25 20:29:57 -0400

    Add files via upload

---
+++ notes_CLI_interface/main.py
@@ -0,0 +1,5 @@
+from program import main
+
+if __name__ == "__main__" :
+    
+    main()
---
+++ notes_CLI_interface/program.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    #I want ot make a Commmand Line Interface (CLI)
+    
+    bookslist == []
+    
+    print(" *** Private Notes Interface *** ")
+    add = print(" Add a book by typing 1!")
+    remove = print(" Remove a book by typing 2!")
+    lookup = print(" Search for a book by pressing 3!")
+    quit = print(" Quit by pressing 4! ")
+    choice = input(">>>")
+    
+    while choice != 4:
+        if choice == "1":
+            
